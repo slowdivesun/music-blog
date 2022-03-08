f41ddf8c5cb96e0dd9f500e4bdc6d67d32aefa64
Shivam <shivam.gup82@gmail.com>
2022-03-08 19:59:02 +0530

    Create, Update profiles, Get all profiles and get profile by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 // Models
 const Author = require("../../models/Author");
@@ -12,7 +13,7 @@ const Profile = require("../../models/Profile");
 router.get("/me", auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ author: req.author.id }).populate(
-      "user",
+      "author",
       ["name"]
     );
 
@@ -22,6 +23,83 @@ router.get("/me", auth, async (req, res) => {
         .json({ msg: "There is no profile for this author" });
     }
 
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @ route  GET api/profile
+// @desc    Create or Update a user
+// @access  Private
+// What if someone wants to update their profile but not their bio
+router.post(
+  "/",
+  [auth, [check("bio", "Bio is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { bio, facebook, twitter, instagram } = req.body;
+
+    const profileFields = {};
+    profileFields.bio = bio;
+    profileFields.author = req.author.id;
+
+    profileFields.social = {};
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (facebook) profileFields.social.facebook = facebook;
+
+    try {
+      let profile = await Profile.findOne({ author: req.author.id });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { author: req.author.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @ route  GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("author", ["name"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @ route  GET api/profile/author/:author_id
+// @desc    Get profile by author ID
+// @access  Public
+router.get("/author/:author_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      author: req.params.author_id,
+    }).populate("author", ["name"]);
+
+    if (!profile) return res.status(400).json({ msg: "No profile found" });
     res.json(profile);
   } catch (err) {
     console.error(err.message);
