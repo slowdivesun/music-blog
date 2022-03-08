76f9920c78f05c9d74676327e4d8f6da13e5a3be
Shivam <shivam.gup82@gmail.com>
2022-03-08 14:21:36 +0530

    Added author scheema

---
+++ models/Author.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const AuthorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Author = mongoose.model("author", AuthorSchema);
