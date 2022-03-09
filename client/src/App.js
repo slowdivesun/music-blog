a3a5019c18d6a85fd78a07c14a4a9d7ff8b840e2
Shivam <shivam.gup82@gmail.com>
2022-03-09 18:43:25 +0530

    Created redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,27 @@ import Login from "./components/auth/Login";
 import Navbar from "./components/layout/Navbar";
 import Reviews from "./components/layout/Reviews";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => {
   return (
-    <div className='App bg-gray-300 h-screen flex flex-col items-stretch'>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component={Reviews} />
-          <section className='App bg-gray-300 h-screen flex flex-col items-center'>
-            <Switch>
-              <Route exact path='/login' component={Login} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className='App bg-gray-300 h-screen flex flex-col items-stretch'>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <Route exact path='/' component={Reviews} />
+            <section className='App bg-gray-300 h-screen flex flex-col items-center'>
+              <Switch>
+                <Route exact path='/login' component={Login} />
+              </Switch>
+            </section>
+          </Fragment>
+        </Router>
+      </div>
+    </Provider>
   );
 };
 
