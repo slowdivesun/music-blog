f863eb37f64abeeda1890a9baa23947b1a9ef70f
Shivam <shivam.gup82@gmail.com>
2022-03-09 11:13:51 +0530

    Added tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Fragment>
+        <Navbar />
+        <h1 className='text-3xl font-bold underline'>App</h1>
+      </Fragment>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div class='p-6 max-w-sm mx-auto bg-white shadow-md flex items-center space-x-4'>
+        Navbar
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
