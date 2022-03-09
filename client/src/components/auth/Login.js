8682dfc1e6dac4c9a29152d03090f4b34a1a8f0d
Shivam <shivam.gup82@gmail.com>
2022-03-09 18:35:32 +0530

    Added login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,50 +1,72 @@
 import React from "react";
+import { useState } from "react";
 
 const Login = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div className='bg-white w-2/3 flex p-6 mt-4'>
-      <form class='w-full max-w-sm'>
-        <div class='md:flex md:items-center mb-6'>
-          <div class='md:w-1/3'>
+    <div className='bg-white flex w-4/6 lg:w-6/12 p-6 mt-4'>
+      <form
+        className='w-full flex flex-col items-center'
+        onSubmit={(e) => onSubmit(e)}
+      >
+        <div className='md:flex md:items-center mb-6'>
+          <div className='md:w-1/3'>
             <label
-              class='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-              for='inline-full-name'
+              className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+              htmlFor='inline-full-name'
             >
-              Full Name
+              Email
             </label>
           </div>
-          <div class='md:w-2/3'>
+          <div className='md:w-2/3'>
             <input
-              class='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+              className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
               id='inline-full-name'
               type='text'
-              value='Jane Doe'
+              name='email'
+              onChange={(e) => onChange(e)}
+              value={email}
             />
           </div>
         </div>
-        <div class='md:flex md:items-center mb-6'>
-          <div class='md:w-1/3'>
+        <div className='md:flex md:items-center mb-6'>
+          <div className='md:w-1/3'>
             <label
-              class='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
-              for='inline-password'
+              className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+              htmlFor='inline-password'
             >
               Password
             </label>
           </div>
-          <div class='md:w-2/3'>
+          <div className='md:w-2/3'>
             <input
-              class='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+              className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
               id='inline-password'
               type='password'
               placeholder='******************'
+              name='password'
+              value={password}
+              onChange={(e) => onChange(e)}
             />
           </div>
         </div>
-        <div class='md:flex md:items-center'>
-          <div class='md:w-1/3'></div>
-          <div class='md:w-2/3'>
+        <div className='md:flex md:items-center'>
+          <div className='md:w-1/3'></div>
+          <div className='md:w-2/3'>
             <button
-              class='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
+              className='shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
               type='button'
             >
               Login
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,10 +5,12 @@ const Navbar = () => {
   return (
     <div>
       <div className='p-6 mx-auto bg-white shadow-md flex items-center space-x-4 w-full flex justify-around sticky'>
-        <div>Pitchfork</div>
-        <Link className='text-xs' to='/login'>
-          WRITE
-        </Link>
+        <div className='flex flex-row items-center w-3/4 justify-between'>
+          <div>Pitchfork</div>
+          <Link className='text-xs' to='/login'>
+            WRITE
+          </Link>
+        </div>
       </div>
     </div>
   );
